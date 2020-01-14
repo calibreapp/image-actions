92f22eb0ec8e42cd87c2567a36ff075d6edee91b
chabad360 <chabad360@gmail.com>
2020-01-14 01:00:44 +0000

    Allow image-actions to run on regular push

--- entrypoint.js
+++ entrypoint.js
@@ -5,25 +5,25 @@ const { GITHUB_TOKEN, GITHUB_EVENT_NAME } = require("./src/constants");
 const githubEvent = require("./src/github-event");
 const run = require("./src/index.js");
 
-if (!GITHUB_TOKEN) {
-  console.log("You must enable the GITHUB_TOKEN secret");
+if (!GITHUB_TOKEN && GITHUB_EVENT_NAME == "pull_request" ) {
+  console.log("::error:: You must provide the GITHUB_TOKEN secret");
   process.exit(1);
 }
 
 const main = async () => {
-  // Bail out if the event that executed the action wasn’t a pull_request
-  if (GITHUB_EVENT_NAME !== "pull_request") {
-    console.log("This action only runs for pushes to PRs");
+  // Bail out if the event that executed the action wasn’t a pull_request or a push
+  if (GITHUB_EVENT_NAME !== "pull_request" && GITHUB_EVENT_NAME != "push") {
+    console.log("::error:: This action only runs for pushes or PRs");
     process.exit(78);
   }
 
   // Bail out if the pull_request event wasn't synchronize or opened
   const event = await githubEvent();
-  if (event.action !== "synchronize" && event.action !== "opened") {
+  if (GITHUB_EVENT_NAME == "pull_request" && event.action !== "synchronize" && event.action !== "opened") {
     console.log(
-      "Check run has action",
+      "::error:: Check run has action",
       event.action,
-      ". Want: synchronize or opened"
+      ". Wants: synchronize or opened"
     );
     process.exit(78);
   }
