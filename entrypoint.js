28ba008d4917f4e19773f3f9f079fc6a0304c9c9
Ben Schwarz <ben@calibreapp.com>
2020-09-15 11:25:08 +1000

    Use dist. +Formatting

--- entrypoint.js
+++ entrypoint.js
@@ -1,38 +1,40 @@
 #!/usr/bin/env node
 
-const { GITHUB_TOKEN, GITHUB_EVENT_NAME, COMPRESS_ONLY } = require("./src/constants");
+const {
+  GITHUB_TOKEN,
+  GITHUB_EVENT_NAME,
+  COMPRESS_ONLY
+} = require('./dist/constants')
 
-const githubEvent = require("./src/github-event");
-const run = require("./src/index.js");
+const githubEvent = require('./dist/github-event')
+const run = require('./dist/index.js')
 
 if (!GITHUB_TOKEN) {
-  console.log("::error:: You must enable the GITHUB_TOKEN secret");
-  process.exit(1);
+  console.log('::error:: You must enable the GITHUB_TOKEN secret')
+  process.exit(1)
 }
 
 const main = async () => {
-
   if (!COMPRESS_ONLY) {
-
     // Bail out if the event that executed the action wasn’t a pull_request
-    if (GITHUB_EVENT_NAME !== "pull_request") {
-      console.log("::error:: This action only runs for pushes to PRs");
-      process.exit(78);
+    if (GITHUB_EVENT_NAME !== 'pull_request') {
+      console.log('::error:: This action only runs for pushes to PRs')
+      process.exit(78)
     }
 
     // Bail out if the pull_request event wasn't synchronize or opened
-    const event = await githubEvent();
-    if (event.action !== "synchronize" && event.action !== "opened") {
+    const event = await githubEvent()
+    if (event.action !== 'synchronize' && event.action !== 'opened') {
       console.log(
-        "::error:: Check run has action",
+        '::error:: Check run has action',
         event.action,
-        ". Wants: synchronize or opened"
-      );
-      process.exit(78);
+        '. Wants: synchronize or opened'
+      )
+      process.exit(78)
     }
   }
 
-  await run();
-};
+  await run()
+}
 
-main();
+main()
