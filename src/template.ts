7c7b040382814008e0a3836af6ff1b9bc2693d60
Ben Schwarz <ben@calibreapp.com>
2020-09-17 14:54:50 +1000

    Use a fully qualified path

--- src/template.ts
+++ src/template.ts
@@ -1,4 +1,4 @@
-import { join } from 'path'
+import { resolve } from 'path'
 
 import ejs from 'ejs'
 
@@ -6,7 +6,7 @@ const TEMPLATE_DIR = 'src/markdown-templates'
 const EJS_OPTIONS = { async: true }
 
 const template = (basename: string, variables: any): Promise<string> => {
-  const filePath = join(TEMPLATE_DIR, basename)
+  const filePath = resolve(TEMPLATE_DIR, basename)
   return ejs.renderFile(filePath, variables, EJS_OPTIONS)
 }
 
